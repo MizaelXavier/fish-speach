473d234a8093f9d1e47374e3d396760d4c3e420a
Mizael1024 <mizael.xaviersantosz@gmail.com>
2025-01-19 16:49:03 -0300

    fish speech

---
+++ pinokio.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Fish Speech",
+  description: "Modelo de síntese de voz Fish Speech",
+  icon: "icon.png",
+  menu: [{
+    html: "Instalar",
+    href: "install.json"
+  }, {
+    html: "Executar",
+    href: "run.json"
+  }]
+} 
